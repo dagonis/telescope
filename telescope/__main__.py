3fe9133af169ae90213b3f50166166c6965d8994
Kevin Tyers <ktyers@gmail.com>
2022-03-14 16:45:04 -0700

    More code

--- telescope/__main__.py
+++ telescope/__main__.py
@@ -25,8 +25,8 @@ def main() -> None:
     parser = argparse.ArgumentParser(description=BANNER)
     # Exclusive Argument Group if you want to either specify a region or if you want to try all regions with your bucket name(s)
     region_group = parser.add_mutually_exclusive_group()
-    region_group.add_argument('--region', "-r",  choices=DIGITAL_OCEAN_REGIONS, help="The Region you want")
-    region_group.add_argument('--all_regions', "-R", action='store_true')
+    region_group.add_argument('--regions', "-r", nargs="+", choices=DIGITAL_OCEAN_REGIONS, help="The Region(s) you want to check.")
+    region_group.add_argument('--all_regions', "-R", action='store_true', help="Try all regions.")
     # Exclusive Argument Group if you want supply a bucket name (or names) at the command line or use a file
     bucket_group = parser.add_mutually_exclusive_group()
     bucket_group.add_argument('--buckets', "-b", nargs="+", help="The bucket(s) you want to check")
@@ -35,14 +35,40 @@ def main() -> None:
     parser.add_argument("--download", action="store_true", help="Download all files, be careful.")
     parser.add_argument("--download_folder", type=str, default=".", help="If --download is used, this where the files will go.")
     args = parser.parse_args()
-    found_files = set()
-    for bucket in args.buckets:
-        bucket_url = f"https://{bucket}.{args.region}.{DIGITAL_OCEAN_SPACES_URL}"
-        results = requests.get(bucket_url).text
-        for result in SPACE_REGEX.findall(results):
-            item = result.split(">")[1].split("<")[0]
-            found_files.add(f"{bucket_url}/{item}")
-    print(found_files)
+    # Get the list of regions that we are going to look into.
+    region_list = []
+    if args.all_regions:
+        region_list = DIGITAL_OCEAN_REGIONS.copy()
+    elif args.regions is not None:
+        region_list = args.regions.copy()
+    else:
+        parser.print_help()
+        print('You must specify at least one region with -r, or all regions with -R.')
+    # Now let's sort out the buckets we will look into.
+    print(args)
+    bucket_set = set()
+    if args.bucket_file is not None:
+        with open(args.bucket_file, 'r') as _buckets:
+            for bucket in _buckets:
+                bucket_set.add(bucket.rstrip()) 
+    elif args.buckets is not None:
+        for bucket in args.buckets:
+            bucket_set.add(bucket)
+    else:
+        parser.print_help()
+        print('You must specify at least one bucket with -b, or a file with buckets with -f')
+    for bucket in bucket_set:
+        for region in region_list:
+            bucket_url = f"https://{bucket}.{region}.{DIGITAL_OCEAN_SPACES_URL}"
+            print(bucket_url)
+    # OLD WAY
+    # for bucket in args.buckets:
+    #     bucket_url = f"https://{bucket}.{args.region}.{DIGITAL_OCEAN_SPACES_URL}"
+    #     results = requests.get(bucket_url).text
+    #     for result in SPACE_REGEX.findall(results):
+    #         item = result.split(">")[1].split("<")[0]
+    #         found_files.add(f"{bucket_url}/{item}")
+    # print(found_files)
 
 
 if __name__ == '__main__':
